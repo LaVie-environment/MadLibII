baf7d18cf4cf71726bf32d6370b0741b66429b93
armzig <bellotaophyc@outlook.com>
2023-08-11 07:28:58 +0100

    generating a new shuffled list from the input list

--- madlibs.tf
+++ madlibs.tf
@@ -23,3 +23,23 @@ variable "words" {
         error_message = "At least 20 nouns must be supplied."
     }
 }
+
+resource "random_shuffle" "random_nouns" {
+    input = var.words["nouns"]
+}
+
+resource "random_shuffle" "random_adjectives" {
+    input = var.words["adjectives"]
+}
+
+resource "random_shuffle" "random_verbs" {
+    input = var.words["verbs"]
+}
+
+resource "random_shuffle" "random_adverbs" {
+    input = var.words["adverbs"]
+}
+
+resource "random_shuffle" "random_numbers" {
+    input = var.words["numbers"]
+}
