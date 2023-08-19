75738207305a5541014ce0af7c596a74824b3e04
armzig <bellotaophyc@outlook.com>
2023-08-19 19:16:26 +0100

    read all the text files from the templates folder into a list

--- madlibs.tf
+++ madlibs.tf
@@ -13,6 +13,10 @@ locals {
     uppercase_words = {for k, v in var.words : k => [for s in v : upper(s)]}
 }
 
+locals {
+    templates = tolist(fileset(path.module, "templates/*.txt"))
+}
+
 resource "random_shuffle" "random_nouns" {
     count = var.num_files
     input = local.uppercase_words["nouns"]
