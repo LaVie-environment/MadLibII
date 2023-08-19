e6e70b0ca97213a4b8822f478805f1967a5e503b
armzig <bellotaophyc@outlook.com>
2023-08-19 19:04:06 +0100

    adding more temlate files to revised our architecture

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 A program that generates Mad Libs paragraphs from template files.
 
-To support producing an arbitrary number of Mad Libs stories. First, add a new variable named var.num_files having type number and a default value of 100.
+To support producing an arbitrary number of Mad Libs stories. First, add a new variable named var.num_files having type number and a default value of 100. Next, reference this variable to dynamically set the count meta argument on each of the shuffle_resources.
+
--- madlibs.tf
+++ madlibs.tf
@@ -14,21 +14,26 @@ locals {
 }
 
 resource "random_shuffle" "random_nouns" {
+    count = var.num_files
     input = local.uppercase_words["nouns"]
 }
 
 resource "random_shuffle" "random_adjectives" {
+    count = var.num_files
     input = local.uppercase_words["adjectives"]
 }
 
 resource "random_shuffle" "random_verbs" {
+    count = var.num_files
     input = local.uppercase_words["verbs"]
 }
 
 resource "random_shuffle" "random_adverbs" {
+    count = var.num_files
     input = local.uppercase_words["adverbs"]
 }
 
 resource "random_shuffle" "random_numbers" {
+    count = var.num_files
     input = local.uppercase_words["numbers"]
 }
