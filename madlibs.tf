92bce3052cd70fd883bae7474e01cacd333d3741
armzig <bellotaophyc@outlook.com>
2023-08-19 19:57:13 +0100

    outputting all the files in the source directory to a new zip file named madlibs.zip

--- madlibs.tf
+++ madlibs.tf
@@ -55,3 +55,10 @@ resource "local_file" "mad_libs" {
         }
     )
 }
+
+data "archive_file" "mad_libs" {
+    depends_on = [local_file.mad_libs]
+    type = "zip"
+    source_dir = "${path.module}/madlibs"
+    output_path = "${path.cwd}/madlibs.zip"
+}
